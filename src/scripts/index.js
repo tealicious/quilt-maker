675ba31027f72d4184aa8e75af4839d1c42ab804
Maggie Meiring <maggiemeiring@Maggies-MacBook-Pro-4.local>
2021-03-10 19:28:34 -0500

    backup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,15 @@
-const gridCount = 16;
-const smallBlockSize = window.innerWidth > window.innerHeight ? window.innerHeight / gridCount :  window.innerWidth / gridCount;
+const gridCount = 4;
+let smallBlockSize
+if (window.innerWidth > window.innerHeight) {
+  smallBlockSize = window.innerHeight / gridCount
+} else {
+  smallBlockSize = window.innerWidth / gridCount;
+}
 const colorA = 255;
 const colorB = 122;
 
 //BLANK or 0 TRIANGLE COMBO
-function drawSquare(x, y, dim){
+function drawSquare(x, y, dim) {
   fill(colorA);
   rect(x, y, dim, dim);
 }
@@ -21,7 +26,7 @@ function drawRightTriangle(x, y, dim) {
   fill(colorA);
   drawSquare(x, y, dim)
   fill(colorB);
-  triangle(x+dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
+  triangle(x + dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
 }
 
 function drawBottomTriangle(x, y, dim) {
@@ -44,7 +49,7 @@ function drawBowtie(x, y, dim) {
   drawSquare(x, y, dim)
   fill(colorB);
   triangle(x, y, x + dim / 2, y + dim / 2, x, y + dim);
-  triangle(x+dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
+  triangle(x + dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
 }
 
 function drawHourGlass(x, y, dim) {
@@ -75,7 +80,7 @@ function drawRightBottom(x, y, dim) {
   fill(colorA);
   drawSquare(x, y, dim)
   fill(colorB);
-  triangle(x+dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
+  triangle(x + dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
   triangle(x, y + dim, x + dim / 2, y + dim / 2, x + dim, y + dim);
 }
 
@@ -83,7 +88,7 @@ function drawRightTop(x, y, dim) {
   fill(colorA);
   drawSquare(x, y, dim)
   fill(colorB);
-  triangle(x+dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
+  triangle(x + dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
   triangle(x, y, x + dim / 2, y + dim / 2, x + dim, y);
 }
 
@@ -101,7 +106,7 @@ function drawSteepleRight(x, y, dim) {
   fill(colorA);
   drawSquare(x, y, dim)
   fill(colorB);
-  triangle(x+dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
+  triangle(x + dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
   triangle(x, y + dim, x + dim / 2, y + dim / 2, x + dim, y + dim);
   triangle(x, y, x + dim / 2, y + dim / 2, x + dim, y);
 }
@@ -111,7 +116,7 @@ function drawSteepleBottom(x, y, dim) {
   drawSquare(x, y, dim)
   fill(colorB);
   triangle(x, y + dim, x + dim / 2, y + dim / 2, x + dim, y + dim);
-  triangle(x+dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
+  triangle(x + dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
   triangle(x, y, x + dim / 2, y + dim / 2, x, y + dim);
 }
 
@@ -120,40 +125,59 @@ function drawSteepleTop(x, y, dim) {
   drawSquare(x, y, dim)
   fill(colorB);
   triangle(x, y, x + dim / 2, y + dim / 2, x + dim, y);
-  triangle(x+dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
+  triangle(x + dim, y, x + dim / 2, y + dim / 2, x + dim, y + dim);
   triangle(x, y, x + dim / 2, y + dim / 2, x, y + dim);
 }
 
 // 4 TRIANGLE COMBO
-function drawFullSquare(x, y, dim){
+function drawFullSquare(x, y, dim) {
   fill(colorB);
   rect(x, y, dim, dim);
 }
 
 
 
-function createLargeBlock(smallBlockSize, corner) {
-  const cornerPosX = (corner % 2 === 0 ? 2 : 0) * smallBlockSize; 
+function createLargeBlock(callbackFunctionArray, smallBlockSize, corner) {
+  const cornerPosX = (corner % 2 === 0 ? 2 : 0) * smallBlockSize;
   const cornerPosY = (corner < 3 ? 0 : 2) * smallBlockSize;
-  console.log(cornerPosX, cornerPosY);
-  return [
-    drawSteepleTop(cornerPosX, cornerPosY, smallBlockSize),
-    drawRightBottom(cornerPosX + smallBlockSize, cornerPosY, smallBlockSize),
-    drawBottomTriangle(cornerPosX, cornerPosY + smallBlockSize, smallBlockSize),
-    drawBowtie(cornerPosX + smallBlockSize, cornerPosY + smallBlockSize, smallBlockSize),
+
+
+
+
+  const positionSet = [
+    [cornerPosX, cornerPosY, smallBlockSize],
+    [cornerPosX + smallBlockSize, cornerPosY, smallBlockSize],
+    [cornerPosX, cornerPosY + smallBlockSize, smallBlockSize],
+    [cornerPosX + smallBlockSize, cornerPosY + smallBlockSize, smallBlockSize],
   ];
+
+  let drawnShapes = []
+  for (let i = 0; i < 4; i++) {
+    drawnShapes[i] = callbackFunctionArray[i](positionSet[i][0], positionSet[i][1], positionSet[i][2])
+  }
+
+  return drawnShapes
+
+}
+
+const shapesToDraw = [
+  drawSteepleBottom,
+  drawSteepleLeft,
+  drawLeftTop,
+  drawSteepleTop
+]
+
+function createFourCorner() {
+  for (let i = 0; i < 4; i++) {
+    createLargeBlock(shapesToDraw, smallBlockSize, i + 1);
+  }
 }
 
 setup = () => {
-createCanvas(gridCount*smallBlockSize, gridCount*smallBlockSize);
-background(0);
-noStroke(0);
+  createCanvas(gridCount * smallBlockSize, gridCount * smallBlockSize);
+  background(0);
+  noStroke(0);
 
-createLargeBlock(smallBlockSize, 1);
-createLargeBlock(smallBlockSize, 2);
-createLargeBlock(smallBlockSize, 3);
-createLargeBlock(smallBlockSize, 4);
-  // for (let i = 0; i < 4; i ++) {
-  //   createLargeBlock(smallBlockSize, i);
-  // }
+
+  createFourCorner();
 };
